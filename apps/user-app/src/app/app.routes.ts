b6be00e980992569635795639e3c41d03dbd93cc
kotsmich <konstantinos.mich91@gmail>
2026-04-07 01:06:15 +0300

    r

--- apps/user-app/src/app/app.routes.ts
+++ apps/user-app/src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const APP_ROUTES: Routes = [
     loadChildren: () => import('./features/home/home.routes').then((m) => m.HOME_ROUTES),
   },
   {
+    path: 'about',
+    loadChildren: () => import('./features/about/about.routes').then((m) => m.ABOUT_ROUTES),
+  },
+  {
     path: 'contact',
     loadChildren: () => import('./features/contact/contact.routes').then((m) => m.CONTACT_ROUTES),
   },
